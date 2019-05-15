f9d1c804e105456f30b9f1342f299b4cb899d2b1
taiduc1988 <taiduc1988@yahoo.com>
2019-05-15 21:41:47 +0700

    [EasyWork] Add structure

---
+++ easy-work/src/main/java/com/example/easywork/EasyWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.easywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWorkApplication.class, args);
+	}
+
+}
